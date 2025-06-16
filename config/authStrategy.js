39d27dceda5e0115190cd565964b1af41275e4b6
jeffrey arbelaez <jeffreyarbelaez@yahoo.com>
2025-06-15 20:26:51 -0500

    3.2 redo

--- config/authStrategy.js
+++ config/authStrategy.js
@@ -1,6 +1,8 @@
 const passport = require("passport");
 const bcrypt = require("bcrypt");
 const LocalStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 
 const User = require("../models/userModel");
 
@@ -30,7 +32,6 @@ passport.use(
   })
 );
 
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 passport.use(
   new GoogleStrategy(
